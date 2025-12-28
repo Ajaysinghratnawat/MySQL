86611d9d7b9272cbc1c74a60e834ec93929544eb
Ajay singh ratnawat <168153835+Ajaysinghratnawat@users.noreply.github.com>
2025-12-28 22:01:46 +0530

    Order by

--- Session_eight.sql
+++ Session_eight.sql
@@ -1,5 +1,5 @@
 use sakila;
-select count(*) from payment where amount=2.99;
+select count(*) from payment where amount=2.99; 
 select amount,count(*) from payment group by amount;
 select customer_id,sum(amount) from payment where amount>3 group by customer_id;
 select customer_id,count(amount) from payment
